16029de0753d8b148f0fc25033617b741cfea199
D9VOL <gladun.an@gmail.com>
2022-07-29 15:04:06 +0300

    lesson2

---
+++ JS/lesson2/main.js
@@ -0,0 +1,135 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+// let brends = ['Audi', 'Bmw', 'Bentley', 'Volkswagen', 'Dodge', 'Jeep', 'Infiniti', 'Kia', 'Lamborghini', 'Mercedes - Benz'];
+// console.log(brends[0])
+// console.log(brends[1])
+// console.log(brends[2])
+// console.log(brends[3])
+// console.log(brends[4])
+// console.log(brends[5])
+// console.log(brends[6])
+// console.log(brends[7])
+// console.log(brends[8])
+// console.log(brends[9])
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
+// const book1 = {
+//     title: 'book1',
+//     pageCount: 12,
+//     genre: 'story',
+// }
+//
+// const book2 = {
+//     title: 'book2',
+//     pageCount: 120,
+//     genre: 'history',
+// }
+//
+// const book3 = {
+//
+//         title: 'book3',
+//         pageCount: 200,
+//         genre: 'fantastic',
+// }
+//
+// console.log(book1,book2,book3);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors.
+// Поле "автори" - масив. Кожен автор має поля name,age
+
+// const book1 = {
+//     title: 'book1',
+//     pageCount: 12,
+//     genre: 'story',
+//     authors: [
+//         {
+//             name: 'Vasya',
+//             age: 30,
+//         }
+//     ]
+// }
+//
+// const book2 = {
+//     title: 'book2',
+//     pageCount: 120,
+//     genre: 'history',
+//     authors: [
+//         {
+//             name: 'ALex',
+//             age: 25,
+//         }
+//     ]
+// }
+//
+// const book3 = {
+//
+//     title: 'book3',
+//     pageCount: 200,
+//     genre: 'fantastic',
+//     authors: [
+//         {
+//             name: 'Misha',
+//             age: 35,
+//         }
+//     ]
+// }
+//
+// console.log(book1,book2,book3);
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. //
+// Вивести в консоль пароль кожного користувача
+
+// let users = [
+//      {
+//          name: 'Andriy',
+//          username: 'D9VOL',
+//          password: 664566,
+//      },{
+//         name: 'Andriy',
+//         username: 'D9VOL',
+//         password: 64645,
+//     },{
+//         name: 'Andriy',
+//         username: 'D9VOL',
+//         password: 12366645456,
+//     },{
+//         name: 'Andriy',
+//         username: 'D9VOL',
+//         password: 1234545456,
+//     },{
+//         name: 'Andriy',
+//         username: 'D9VOL',
+//         password: 1266453456,
+//     },{
+//         name: 'Andriy',
+//         username: 'D9VOL',
+//         password: 145454523456,
+//     },{
+//         name: 'Andriy',
+//         username: 'D9VOL',
+//         password: 1245453456,
+//     },{
+//         name: 'Andriy',
+//         username: 'D9VOL',
+//         password: 127745453456,
+//     },{
+//         name: 'Andriy',
+//         username: 'D9VOL',
+//         password: 1245453456,
+//     },{
+//         name: 'Andriy',
+//         username: 'D9VOL',
+//         password: 56,
+//     }
+// ]
+// console.log(users[0].password);
+// console.log(users[1].password);
+// console.log(users[2].password);
+// console.log(users[3].password);
+// console.log(users[4].password);
+// console.log(users[5].password);
+// console.log(users[6].password);
+// console.log(users[7].password);
+// console.log(users[8].password);
+// console.log(users[9].password);
